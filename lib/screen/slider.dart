686d97fc4c42713921097a9aa0150bbf6090f4f0
utkarshjain-spec <63394645+utkarshjain-spec@users.noreply.github.com>
2021-07-20 09:24:59 +0530

    slider file updated

--- lib/screen/slider.dart
+++ lib/screen/slider.dart
@@ -1,8 +1,9 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 final List<String> imgList = [
-  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1600147131759-880e94a6185f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cXIlMjBjb2RlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
   'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
 ];
 
@@ -24,60 +25,114 @@ class _CarouselWithIndicatorState extends State<CarouselWithIndicatorDemo> {
     final double height = MediaQuery.of(context).size.height;
     return SafeArea(
       child: Scaffold(
-        body: Column(children: [
-          Expanded(
-            child: CarouselSlider(
-              items: imageSliders,
-              carouselController: _controller,
-              options: CarouselOptions(
-                  height: height,
-                  autoPlay: true,
-                  enlargeCenterPage: false,
-                  viewportFraction: 1.0,
-                  onPageChanged: (index, reason) {
-                    setState(() {
-                      _current = index;
-                    });
-                  }),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: imgList.asMap().entries.map((entry) {
-              return GestureDetector(
-                onTap: () => _controller.animateToPage(entry.key),
-                child: Container(
-                  width: 12.0,
-                  height: 12.0,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: (Theme.of(context).brightness == Brightness.dark
-                              ? Colors.white
-                              : Colors.black)
-                          .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+        body: Stack(
+          children: [
+            Column(children: [
+              Expanded(
+                child: CarouselSlider(
+                  items: imageSliders,
+                  carouselController: _controller,
+                  options: CarouselOptions(
+                      height: height,
+                      autoPlay: true,
+                      enlargeCenterPage: true,
+                      viewportFraction: 1.0,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          _current = index;
+                        });
+                      }),
                 ),
-              );
-            }).toList(),
-          ),
-        ]),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: imgList.asMap().entries.map((entry) {
+                  return GestureDetector(
+                    onTap: () => _controller.animateToPage(entry.key),
+                    child: Container(
+                      margin: EdgeInsets.only(top: 5.0),
+                      width: 6.0,
+                      height: 6.0,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: (Theme.of(context).brightness ==
+                                      Brightness.dark
+                                  ? Colors.white
+                                  : Colors.black)
+                              .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+                    ),
+                  );
+                }).toList(),
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(67.0, 60.0, 65.0, 144.0),
+                  width: 243.0,
+                  height: 52.0,
+                  padding: EdgeInsets.fromLTRB(43.0, 15.0, 45.84, 11.48),
+                  child: Center(
+                    child: Text(
+                      "Get Started >",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Color(0xffDD4C00),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+              ),
+            ]),
+            Container(
+                child: Image.asset("assets/images/logo.png"),
+                width: 60.0,
+                height: 60.0,
+                margin: EdgeInsets.only(top: 32, left: 24)),
+            Container(
+              width: 269.0,
+              height: 9.5,
+              color: Colors.white,
+              margin: EdgeInsets.only(top: 374, left: 53, right: 53),
+            ),
+            Container(
+              width: 269.0,
+              height: 9.5,
+              color: Colors.white,
+              margin: EdgeInsets.only(top: 394, left: 53, right: 53),
+            ),
+            Container(
+              width: 269.0,
+              height: 9.5,
+              color: Colors.white,
+              margin: EdgeInsets.only(top: 414, left: 86, right: 86),
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
 final List<Widget> imageSliders = imgList
-    .map((item) => Container(
-          child: Container(
-            child: ClipRRect(
-                child: Stack(
-              children: <Widget>[
-                Image.network(
-                  item,
-                  fit: BoxFit.cover,
-                  width: 1000.0,
-                ),
-              ],
-            )),
-          ),
-        ))
+    .map(
+      (item) => Container(
+        child: Container(
+          child: ClipRRect(
+              child: Stack(
+            children: <Widget>[
+              Image.network(
+                item,
+                fit: BoxFit.cover,
+                height: 553.0,
+                width: 1000.0,
+              ),
+            ],
+          )),
+        ),
+      ),
+    )
     .toList();
