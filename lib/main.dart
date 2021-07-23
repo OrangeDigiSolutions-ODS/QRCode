0e3e449bb2d949bf465d97ebeb42a26810615fe4
Sudarshan Singh <64999011+sudarshansingh046@users.noreply.github.com>
2021-07-23 14:27:37 +0530

    check update

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.orange,
       ),
       home: MyHomePage(),
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
